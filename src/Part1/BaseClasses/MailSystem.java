b598abdf39f740b6adfa9e5a5405c255ece2d743
AlbertCS <albert.canellas@estudiants.urv.cat>
2021-01-08 17:35:10 +0100

    Added a hashmap in mailSystem

--- src/Part1/BaseClasses/MailSystem.java
+++ src/Part1/BaseClasses/MailSystem.java
@@ -2,10 +2,7 @@ package Part1.BaseClasses;
 
 import Part1.FunctionFilter;
 
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -13,8 +10,7 @@ import java.util.stream.Stream;
 
 public class MailSystem {
 
-    private LinkedList<User> users = new LinkedList<>();
-    private LinkedList<MailBox> mailBoxes = new LinkedList<>();
+    private HashMap<User, MailBox> dictionary = new HashMap<>();
     private MailStore mailStore;
 
     public MailSystem(MailStore mailStore) {
@@ -24,47 +20,48 @@ public class MailSystem {
     public MailBox createUser(String userName, String name, int birthYear){
         User user = new User(userName, name, birthYear);
         MailBox mailBox = new MailBox(mailStore, user);
-        users.add(user);
-        mailBoxes.add(mailBox);
+        dictionary.put(user, mailBox);
         return mailBox;
     }
 
     public boolean userExist(String userName){
-        return mailBoxes.stream().anyMatch(mailBox -> mailBox.getUser().getUserName().equals(userName));
+        return dictionary.entrySet().stream().anyMatch(map -> map.getKey().getUserName().equals(userName));
     }
 
     public MailBox getMailBox(String userName){
-        return mailBoxes.stream().filter(mailBox -> mailBox.getUser().getUserName().equals(userName)).collect(CustomCollector.toSingleton());
+        return dictionary.get(dictionary.keySet().stream().filter(user -> user.getUserName().equals(userName))
+                .collect(CustomCollector.toSingleton()));
     }
 
     public void updateAllMessages(Comparator comparator){
-        mailBoxes.stream().forEach(mailBox -> mailBox.update(comparator));
+        dictionary.entrySet().stream().forEach(map -> map.getValue().update(comparator));
     }
 
     public void getAllMessages(){
-        mailBoxes.stream().forEach(MailBox::listMail);
+        dictionary.entrySet().stream().forEach(map -> map.getValue().listMail());
     }
 
     public void getAllUsers(){
         System.out.println("\nAll users:");
-        users.stream().forEach(user -> System.out.println(user.getUserName()));
+        dictionary.keySet().stream().forEach(System.out::println);
     }
 
     public LinkedList<Message> filterAllMessagesSingleWord(){
         LinkedList<Message> messages =
-                mailBoxes.stream().map(MailBox::getReceivedMessages).flatMap(l -> l.stream()).collect(Collectors.toCollection(LinkedList::new));
+                dictionary.entrySet().stream().map(map -> map.getValue().getReceivedMessages())
+                        .flatMap(l -> l.stream()).collect(Collectors.toCollection(LinkedList::new));
         return messages.stream().filter(message -> message.getSubject().split("\\w+").length == 0).collect(Collectors.toCollection(LinkedList::new));
     }
 
 
-
     public LinkedList<Message> filterAllMessagesYear(Predicate predicate){
-
         LinkedList<Message> messages =
-                mailBoxes.stream().map(MailBox::getReceivedMessages).flatMap(l -> l.stream()).collect(Collectors.toCollection(LinkedList::new));
+                dictionary.entrySet().stream().map(map -> map.getValue().getReceivedMessages())
+                        .flatMap(l -> l.stream()).collect(Collectors.toCollection(LinkedList::new));
+
         LinkedList<User> users2000 =
-                (LinkedList<User>) users.stream().filter(predicate).collect(Collectors.toCollection(LinkedList::new));
-        //users.stream().filter( user -> user.getBirthYear() > 2000).collect(Collectors.toCollection(LinkedList::new));
+                (LinkedList<User>) dictionary.keySet().stream().filter(predicate).collect(Collectors.toCollection(LinkedList::new));
+
         LinkedList<Message> messagesResu= new LinkedList<>();
         for(User user:users2000){
             messages.stream().filter(message -> message.getSender().equals(user.getUserName())).sequential().collect(Collectors.toCollection(() -> messagesResu));
@@ -73,18 +70,19 @@ public class MailSystem {
     }
 
     public void countMessages(){
-        long num = mailBoxes.stream().map(MailBox::getReceivedMessages).flatMap(l -> l.stream()).count();
+        long num = dictionary.entrySet().stream().map(map -> map.getValue().getReceivedMessages()).flatMap(l  -> l.stream()).count();
         System.out.println("\nThe total number of messages is " +num);
     }
 
     public  void  avgMessages(){
-        long num = mailBoxes.stream().map(MailBox::getReceivedMessages).flatMap(l -> l.stream()).count();
-        System.out.println("\nThe average of messages is " +(num/ users.size()));
+        long num = dictionary.entrySet().stream().map(map -> map.getValue().getReceivedMessages()).flatMap(l  -> l.stream()).count();
+        System.out.println("\nThe average of messages is " +(num / dictionary.size()));
     }
 
     public Map<String, List<Message>> grupSubject(){
         LinkedList<Message> messages =
-                mailBoxes.stream().map(MailBox::getReceivedMessages).flatMap(l -> l.stream()).collect(Collectors.toCollection(LinkedList::new));
+                dictionary.entrySet().stream().map(map -> map.getValue().getReceivedMessages())
+                        .flatMap(l -> l.stream()).collect(Collectors.toCollection(LinkedList::new));
         Map<String, List<Message>> bySubject
                 = messages.stream()
                 .collect(Collectors.groupingBy(Message::getSubject));
@@ -93,8 +91,9 @@ public class MailSystem {
 
     public int countWords(String userName){
         LinkedList<Message> messages =
-                mailBoxes.stream().map(MailBox::getReceivedMessages).flatMap(l -> l.stream())
-                        .filter(message -> message.getSender().equalsIgnoreCase(userName)).collect(Collectors.toCollection(LinkedList::new));
+                dictionary.entrySet().stream().map(map -> map.getValue().getReceivedMessages())
+                        .flatMap(l -> l.stream()).filter(message -> message.getSender().equalsIgnoreCase(userName))
+                        .collect(Collectors.toCollection(LinkedList::new));
         int words=0;
         for(Message message:messages){
             words += message.getBody().split("\\s+").length;
